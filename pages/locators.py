2b2ab607048bc68bc9370f3767c5ca5f03cd432f
F_Ksu <ksusha342@yandex.ru>
2020-06-17 13:16:32 +0300

    add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+
+class ProductPageLocators:
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_AVAILABILITY = (By.CSS_SELECTOR, ".product_main .instock")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success:nth-child(1) .alertinner strong")
+    STOCK_SIZE = (By.CSS_SELECTOR, ".alertinner p strong")
