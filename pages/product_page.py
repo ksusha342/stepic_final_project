244faea4cc7cb9b67323329787659fd00712cd48
F_Ksu <ksusha342@yandex.ru>
2020-06-17 19:41:00 +0300

    update file structure

--- pages/product_page.py
+++ pages/product_page.py
@@ -19,6 +19,13 @@ class ProductPage(BasePage):
         self.should_be_success_message_with_product_name()
         self.should_be_success_message_with_stock_size()
 
+    def should_be_available_in_stock(self):
+        product_availability = self.is_element_present(*ProductPageLocators.PRODUCT_AVAILABILITY)
+        assert product_availability, 'Product is not available in stock'
+
+    def should_be_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_BUTTON), 'Basket button for product is not found'
+
     def should_be_product_name(self):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), 'Product name is not found'
         self.product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
@@ -27,12 +34,11 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), 'Price of product is not found'
         self.product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
 
-    def should_be_available_in_stock(self):
-        product_availability = self.is_element_present(*ProductPageLocators.PRODUCT_AVAILABILITY)
-        assert product_availability, 'Product is not available in stock'
-
-    def should_be_basket_button(self):
-        assert self.is_element_present(*ProductPageLocators.BASKET_BUTTON), 'Basket button for product is not found'
+    def should_be_success_message_after_adding_product_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        button.click()
+        assert self.is_not_element_present(
+            *ProductPageLocators.SUCCESS_MESSAGE), 'Success message is presented, but should not be'
 
     def should_be_success_message_with_product_name(self):
         product_name_in_success_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
@@ -42,17 +48,11 @@ class ProductPage(BasePage):
         stock_size = self.browser.find_element(*ProductPageLocators.STOCK_SIZE).text
         assert stock_size == self.product_price, 'Stock size is not correct'
 
-    def should_not_be_success_message(self):
-        assert self.is_not_element_present(
-            *ProductPageLocators.SUCCESS_MESSAGE), 'Success message is presented, but should not be'
-
-    def should_be_success_message_after_adding_product_to_basket(self):
-        button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
-        button.click()
-        assert self.is_not_element_present(
-            *ProductPageLocators.SUCCESS_MESSAGE), 'Success message is presented, but should not be'
-
     def should_disappear_of_success_message(self):
         button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         button.click()
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), 'Success message is not disappeared, but should be'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(
+            *ProductPageLocators.SUCCESS_MESSAGE), 'Success message is presented, but should not be'
