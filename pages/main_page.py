48fe31c16b4ab3361bbf71c3622d6c3d01293215
F_Ksu <ksusha342@yandex.ru>
2020-06-17 17:15:07 +0300

    add function should_see_product_in_basket_opened_from_main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,9 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .locators import BasePageLocators
 
 
 class MainPage(BasePage):
-    pass
+    def should_see_product_in_basket_opened_from_main_page(self,):
+        self.browser.find_element(*BasePageLocators.BASKET).click()
+        assert self.is_not_element_present(*BasePageLocators.PRODUCT_BASKET), 'Product(s) in basket are presented, but should not be'
