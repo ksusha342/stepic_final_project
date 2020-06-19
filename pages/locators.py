5433b9b0546dc03cd6feaf57477e179335989bd6
F_Ksu <ksusha342@yandex.ru>
2020-06-19 16:04:56 +0300

    add new locators for LoginPage

--- pages/locators.py
+++ pages/locators.py
@@ -7,9 +7,10 @@ class BasketPageLocators:
 
 
 class BasePageLocators:
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group .btn.btn-default:nth-child(1)")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group .btn.btn-default:nth-child(1)")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class MainPageLocators:
@@ -18,13 +19,17 @@ class MainPageLocators:
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '[name = "registration_submit"]')
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    USER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    USER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    USER_PASSWORD_REPEAT = (By.CSS_SELECTOR, "#id_registration-password2")
 
 
 class ProductPageLocators:
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_AVAILABILITY = (By.CSS_SELECTOR, ".product_main .instock")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    PRODUCT_AVAILABILITY = (By.CSS_SELECTOR, ".product_main .instock")
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success:nth-child(1) .alertinner strong")
     STOCK_SIZE = (By.CSS_SELECTOR, ".alertinner p strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success:nth-child(1) .alertinner strong")
