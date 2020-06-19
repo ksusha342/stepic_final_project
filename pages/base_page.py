441fdd5d37658ed368b4298d443841be8c0787a5
F_Ksu <ksusha342@yandex.ru>
2020-06-19 16:28:04 +0300

    add method should_be_authorized_user

--- pages/base_page.py
+++ pages/base_page.py
@@ -16,8 +16,10 @@ class BasePage:
         #self.browser.implicitly_wait(timeout)
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
-        login_link.click()
+        self.browser.find_element(*BasePageLocators.LOGIN_LINK).click()
+
+    def go_to_basket_page(self):
+        self.browser.find_element(*BasePageLocators.BASKET_LINK).click()
 
     def is_disappeared(self, how, what, timeout=4):
         try:
@@ -43,6 +45,10 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                 " probably unauthorised user"
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
