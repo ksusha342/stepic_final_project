b9ed88165652f92264bbd9adc43ef11c84d73e43
F_Ksu <ksusha342@yandex.ru>
2020-06-19 16:04:06 +0300

    add function register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,12 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.USER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.USER_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.USER_PASSWORD_REPEAT).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -16,3 +22,4 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
