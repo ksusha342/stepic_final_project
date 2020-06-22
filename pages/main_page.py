a6c5cf8be987bfacca295a4a2e450663761e7254
F_Ksu <ksusha342@yandex.ru>
2020-06-22 10:51:59 +0300

    delete commented code

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,5 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
-from .locators import BasePageLocators
 
 
 class MainPage(BasePage):
-    def should_see_product_in_basket_opened_from_main_page(self,):
-        self.browser.find_element(*BasePageLocators.BASKET).click()
-        assert self.is_not_element_present(*BasePageLocators.PRODUCT_BASKET), 'Product(s) in basket are presented, but should not be'
+    pass
